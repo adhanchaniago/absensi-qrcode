d6812eb853c45c5b403782e5ed6ffce3a5597d54
Adi Christian <34901010+adikchristian@users.noreply.github.com>
2020-07-18 12:00:22 +0800

    Fix Log Absen Mingguan

--- fungsi.php
+++ fungsi.php
@@ -1,32 +1,71 @@
 <?php
-include"config.php";
+include "config.php";
 
-function HariIndo($hari){
-    if($hari=="Monday"){
-        $namahari="Senin";
-    }else if($hari=="Tuesday"){
-        $namahari="Selasa";
-    }else if($hari=="Wednesday"){
-        $namahari="Rabu";
-    }else if($hari=="Thursday"){
-        $namahari="Kamis";
-    }else if($hari=="Friday"){
-        $namahari="Jumat";
-    }else if($hari=="Saturday"){
-        $namahari="Sabtu";
-    }else if($hari=="Sunday"){
-        $namahari="Minggu";
+function HariIndo($hari)
+{
+    if ($hari == "Monday") {
+        $namahari = "Senin";
+    } else if ($hari == "Tuesday") {
+        $namahari = "Selasa";
+    }
+    else if ($hari == "Wednesday") {
+        $namahari = "Rabu";
+    }
+    else if ($hari == "Thursday") {
+        $namahari = "Kamis";
+    }
+    else if ($hari == "Friday") {
+        $namahari = "Jumat";
+    }
+    else if ($hari == "Saturday") {
+        $namahari = "Sabtu";
+    }
+    else if ($hari == "Sunday") {
+        $namahari = "Minggu";
     }
     return $namahari;
 }
 
 function GetValidasi($val)
 {
-    if($val=="1"){
-        $validasi="YA";
-    }else{
-        $validasi="TIDAK";
+    if ($val == "1") {
+        $validasi = "YA";
+    } else {
+        $validasi = "TIDAK";
     }
     return $validasi;
 }
-?>
+
+//$tanggal='2011-07-18';
+
+//$minggu_ke = weekNumberOfMonth($tanggal);
+
+//echo ($tanggal . " adalah minggu ke=" . $minggu_ke);
+
+function weekNumberOfMonth($date)
+{
+
+    $tgl = date_parse($date);
+
+    $tanggal =  $tgl['day'];
+
+    $bulan   =  $tgl['month'];
+
+    $tahun   =  $tgl['year'];
+
+    //tanggal 1 tiap bulan
+
+    $tanggalAwalBulan = mktime(0, 0, 0, $bulan, 1, $tahun);
+
+    $mingguAwalBulan = (int) date('W', $tanggalAwalBulan);
+
+    //tanggal sekarang
+
+    $tanggalYangDicari = mktime(0, 0, 0, $bulan, $tanggal, $tahun);
+
+    $mingguTanggalYangDicari = (int) date('W', $tanggalYangDicari);
+
+    $mingguKe = $mingguTanggalYangDicari - $mingguAwalBulan + 1;
+
+    return $mingguKe;
+}
--- page/materi/index.php
+++ page/materi/index.php
@@ -1,7 +1,8 @@
 <?php
 include "../../header.php";
 include "../../sidebar.php";
-$link= $linkglobal.'page/materi/';
+include "../../fungsi.php";
+$link = $linkglobal . 'page/materi/';
 ?>
 <!-- MAIN -->
 <div class="main">
@@ -11,6 +12,10 @@ $link= $linkglobal.'page/materi/';
             <!-- OVERVIEW -->
             <div class="panel panel-headline">
                 <div class="panel-heading">
+                    <?php
+                        $tglsekarang=date('Y-m-d');
+                        $mingguSekarang = date('W', strtotime($tglsekarang));
+                    ?>
                     <h3 class="panel-title">Materi Pengajaran</h3>
                     <div class="right">
                         <a href="<?php echo $link; ?>add.php" class="btn btn-success">Tambah Data</a>
@@ -31,32 +36,100 @@ $link= $linkglobal.'page/materi/';
                             </thead>
                             <tbody>
                                 <?php
-                                    $sql=$DB_CON->prepare("SELECT matakuliah.kode_matakuliah,matakuliah.nama_matakuliah,materi.id_materi,materi.tgl_materi,materi.judul_materi FROM materi LEFT OUTER JOIN mengajar ON(mengajar.id_mengajar=materi.id_mengajar) LEFT OUTER JOIN matakuliah ON(matakuliah.kode_matakuliah=mengajar.kode_matakuliah) LEFT OUTER JOIN dosen ON(dosen.id_dosen=mengajar.id_dosen) WHERE mengajar.id_dosen='$_SESSION[qrid]' AND materi.visible='1'");
-                                    $sql->execute();
-                                    if($sql->rowCount() > 0){
-                                        $no=1;
-                                        while($hasil=$sql->fetch(PDO::FETCH_ASSOC)){
+                                $sql = $DB_CON->prepare("SELECT matakuliah.kode_matakuliah,matakuliah.nama_matakuliah,materi.id_materi,materi.tgl_materi,materi.judul_materi FROM materi LEFT OUTER JOIN mengajar ON(mengajar.id_mengajar=materi.id_mengajar) LEFT OUTER JOIN matakuliah ON(matakuliah.kode_matakuliah=mengajar.kode_matakuliah) LEFT OUTER JOIN dosen ON(dosen.id_dosen=mengajar.id_dosen) WHERE mengajar.id_dosen='$_SESSION[qrid]' AND materi.visible='1'");
+                                $sql->execute();
+                                if ($sql->rowCount() > 0) {
+                                    $no = 1;
+                                    while ($hasil = $sql->fetch(PDO::FETCH_ASSOC)) {
                                 ?>
-                                <tr>
-                                    <td><?php echo $no; ?></td>
-                                    <td><?php echo $hasil['kode_matakuliah']; ?></td>
-                                    <td><?php echo $hasil['nama_matakuliah']; ?></td>
-                                    <td><?php echo $hasil['tgl_materi']; ?></td>
-                                    <td><?php echo $hasil['judul_materi']; ?></td>
-                                    <td>
-                                        <a onclick="return Tanya()" href="<?php echo $link; ?>index.php?hapus=<?php echo $hasil['id_materi']; ?>" class="btn btn-danger btn-sm"><i class="lnr lnr-trash"></i></a>
-                                    </td>
-                                </tr>
+                                        <tr>
+                                            <td><?php echo $no; ?></td>
+                                            <td><?php echo $hasil['kode_matakuliah']; ?></td>
+                                            <td><?php echo $hasil['nama_matakuliah']; ?></td>
+                                            <td><?php echo $hasil['tgl_materi']; ?></td>
+                                            <td><?php echo $hasil['judul_materi']; ?></td>
+                                            <td>
+                                                <a onclick="return Tanya()" href="<?php echo $link; ?>index.php?hapus=<?php echo $hasil['id_materi']; ?>" class="btn btn-danger btn-sm"><i class="lnr lnr-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                        $no++;
+                                    }
+                                } else {
+                                    ?>
+                                    <tr>
+                                        <td colspan="6" align="center"><b>Data Kosong</b></td>
+                                    </tr>
                                 <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- END OVERVIEW -->
+
+            <!-- OVERVIEW -->
+            <div class="panel panel-headline">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Log Absensi Minggu ini</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Tanggal</th>
+                                    <th>Nim</th>
+                                    <th>Nama</th>
+                                    <th>Matakuliah</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = $DB_CON->prepare("SELECT absensi.tgl_absensi,mahasiswa.nim_mahasiswa,mahasiswa.nama_mahasiswa,matakuliah.nama_matakuliah 
+                                FROM `absensi` 
+                                LEFT OUTER JOIN kelas_mhs ON(kelas_mhs.id_kelas_mhs=absensi.id_kelas)
+                                LEFT OUTER JOIN mahasiswa ON(mahasiswa.nim_mahasiswa=kelas_mhs.nim_mahasiswa)
+                                LEFT OUTER JOIN mengajar ON(mengajar.id_mengajar=kelas_mhs.id_mengajar)
+                                LEFT OUTER JOIN matakuliah ON(mengajar.kode_matakuliah=matakuliah.kode_matakuliah)
+                                WHERE absensi.validasi='1' and mengajar.id_dosen='$_SESSION[qrid]'");
+                                $sql->execute();
+                                if ($sql->rowCount() > 0) {
+                                    $no = 1;
+                                    $i= 0 ;
+                                    while ($hasil = $sql->fetch(PDO::FETCH_ASSOC)) {
+                                        $minggutgl = date('W', strtotime($hasil['tgl_absensi']));
+                                        if($minggutgl==$mingguSekarang){
+                                            $i++;
+                                ?>
+                                        <tr>
+                                            <td><?php echo $no; ?></td>
+                                            <td><?php echo $hasil['tgl_absensi']; ?></td>
+                                            <td><?php echo $hasil['nim_mahasiswa']; ?></td>
+                                            <td><?php echo $hasil['nama_mahasiswa']; ?></td>
+                                            <td><?php echo $hasil['nama_matakuliah']; ?></td>
+                                        </tr>
+                                    <?php
                                         $no++;
                                         }
-                                    }else{
-                                ?>
-                                <tr>
-                                    <td colspan="6" align="center"><b>Data Kosong</b></td>
-                                </tr>
-                                <?php
                                     }
+                                if($i==0){
+                                ?>
+                                     <tr>
+                                        <td colspan="6" align="center"><b>Data Kosong</b></td>
+                                    </tr>
+                                <?php
+                                }
+                                } else {
+                                    ?>
+                                    <tr>
+                                        <td colspan="6" align="center"><b>Data Kosong</b></td>
+                                    </tr>
+                                <?php
+                                }
                                 ?>
                             </tbody>
                         </table>
@@ -71,12 +144,12 @@ $link= $linkglobal.'page/materi/';
 <!-- END MAIN -->
 <?php include "../../footer.php"; ?>
 <?php
-if(isset($_GET['hapus'])){
-    $sqlhapus=$DB_CON->prepare("UPDATE materi SET visible='0' WHERE id_materi='$_GET[hapus]'");
+if (isset($_GET['hapus'])) {
+    $sqlhapus = $DB_CON->prepare("UPDATE materi SET visible='0' WHERE id_materi='$_GET[hapus]'");
     $sqlhapus->execute();
-    if($sqlhapus){
+    if ($sqlhapus) {
         header("location:index.php");
-    }else{
+    } else {
         echo "<script>alert('Erorr Gagal Hapus Data !!!');</script>";
     }
 }
